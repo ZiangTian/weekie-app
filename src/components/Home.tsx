7f08e7af17fa9a427adcbbc0164a238abf42962c
tza <bienlor@163.com>
2024-06-08 18:59:35 +0800

    merged with local

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,3 @@
-// import React from 'react';
-// import { Scheduler } from '@aldabil/react-scheduler';
-// import { EVENTS } from '../events';
-
-// const Home: React.FC = () => {
-//   return (
-//     <div>
-//       <h2>Home</h2>
-//       <p>Welcome to the Home page!</p>
-//       <div style={{width: '100%', height: '100%', background: '#F4BE4B'}}> sadjlk </div> 
-//       <Scheduler events={EVENTS} />
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Menu from '../components/Menu'; 
